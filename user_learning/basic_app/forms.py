a969f17ca4f22be6ea566eee944d34c22dc99ab8
tayyabfarooq800 <tayyabsab800@gmail.com>
2021-10-05 14:11:18 +0500

    taimoor

---
+++ user_learning/basic_app/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.forms import fields
+from basic_app.models import UserProfileInfo
+from django import forms
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+
+        model = User
+        fields = ("username" , "email" , "password")
+
+
+class UserProfileInfoForm(forms.ModelForm):
+
+    class Meta():
+        model = UserProfileInfo
+        fields = ("portfolio_site" , "profile_pic")
---
+++ user_learning/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import login
+from django.urls import path
+from basic_app import views
+
+app_name ="basic_app"
+
+urlpatterns  = [
+    path("register/", views.register , name = "register"),
+    path("user_login/" , views.user_login , name = "user_login")
+]
